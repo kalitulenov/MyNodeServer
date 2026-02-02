96b29802192f7f7fd4fdcd61ad484f313f16f984
kalitulenov <kalitulenov@mail.ru>
2026-02-01 20:29:41 -0500

    01.02.2026-4

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
+const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
 app.use(cors());
